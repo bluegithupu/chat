a9b238a9bd1d6abd92501f122af863405ecfbe40
Blue <304584817@qq.com>
2018-04-30 14:15:46 +0800

    two

---
+++ src/test/java/com/game/ChatClientTest.java
@@ -0,0 +1,20 @@
+package com.game;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple ChatClient.
+ */
+public class ChatClientTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
